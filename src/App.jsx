1ccc7541fe68ab5ea5853fa4ac263ed606327cbf
Momen-G-Ar-Official <cp.momen@gmail.com>
2023-01-01 12:24:24 +0200

    add the antd library and start the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home-page/home.page';
 
 const App = () => {
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<></>} />
+                    <Route path='/*' element={<HomePage />} />
+                    <Route path='/home-page' element={<HomePage />} />
                 </Routes>
             </BrowserRouter>
         </div>
