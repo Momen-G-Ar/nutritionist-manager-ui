d33e765346d789954e827c09551b57661dd53ff8
Momen-G-Ar-Official <cp.momen@gmail.com>
2023-01-05 19:46:38 +0200

    small change & edit new program page

--- src/pages/new-program/new-program.page.jsx
+++ src/pages/new-program/new-program.page.jsx
@@ -5,7 +5,9 @@ import React from 'react';
 const NewProgram = () => {
     return (
         <div className='newProgram'>
-            
+            <div className='statistics'>
+                &nbsp;&nbsp;&nbsp;&nbsp;Statistics
+            </div>
         </div>
     );
 };
